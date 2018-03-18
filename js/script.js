e5c077999555a53b0a737d02fa7c495aad15654e
Chris Diana <cdiana.media@gmail.com>
2018-03-18 10:58:57 -0600

    update content

--- js/script.js
+++ js/script.js
@@ -1,20 +1,7 @@
-/*
-
-Style   : MobApp Script JS
-Version : 1.0
-Author  : Surjith S M
-URI     : https://surjithctly.in/
-
-Copyright © All rights Reserved
-
-*/
-
 $(function() {
     "use strict";
 
-    /*-----------------------------------
-     * FIXED  MENU - HEADER
-     *-----------------------------------*/
+    // Menu Header
     function menuscroll() {
         var $navmenu = $('.nav-menu');
         if ($(window).scrollTop() > 50) {
@@ -27,16 +14,13 @@ $(function() {
     $(window).on('scroll', function() {
         menuscroll();
     });
-    /*-----------------------------------
-     * NAVBAR CLOSE ON CLICK
-     *-----------------------------------*/
 
+    // Navbar close on click
     $('.navbar-nav > li:not(.dropdown) > a').on('click', function() {
         $('.navbar-collapse').collapse('hide');
     });
-    /*
-     * NAVBAR TOGGLE BG
-     *-----------------*/
+
+    // Navbar toggle background
     var siteNav = $('#navbar');
     siteNav.on('show.bs.collapse', function(e) {
         $(this).parents('.nav-menu').addClass('menu-is-open');
@@ -45,40 +29,30 @@ $(function() {
         $(this).parents('.nav-menu').removeClass('menu-is-open');
     })
 
-    /*-----------------------------------
-     * ONE PAGE SCROLLING
-     *-----------------------------------*/
-    // Select all links with hashes
+    // Scrolling
     $('a[href*="#"]').not('[href="#"]').not('[href="#0"]').not('[data-toggle="tab"]').on('click', function(event) {
-        // On-page links
         if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
-            // Figure out element to scroll to
             var target = $(this.hash);
             target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-            // Does a scroll target exist?
             if (target.length) {
-                // Only prevent default if animation is actually gonna happen
                 event.preventDefault();
                 $('html, body').animate({
                     scrollTop: target.offset().top
                 }, 1000, function() {
-                    // Callback after animation
-                    // Must change focus!
                     var $target = $(target);
                     $target.focus();
-                    if ($target.is(":focus")) { // Checking if the target was focused
+                    if ($target.is(":focus")) {
                         return false;
                     } else {
-                        $target.attr('tabindex', '-1'); // Adding tabindex for elements not focusable
-                        $target.focus(); // Set focus again
+                        $target.attr('tabindex', '-1');
+                        $target.focus();
                     };
                 });
             }
         }
     });
-    /*-----------------------------------
-     * OWL CAROUSEL
-     *-----------------------------------*/
+
+    // Carousel
     var $testimonialsDiv = $('.testimonials');
     if ($testimonialsDiv.length && $.fn.owlCarousel) {
         $testimonialsDiv.owlCarousel({
